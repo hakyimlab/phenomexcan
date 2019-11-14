5b8673be4a500c785fdc605ef4ff72eb86b67e5f
Milton Pividori <miltondp@uchicago.edu>
2019-11-14 14:29:10 -0600

    20_smultixcan_fastenloc_stats.ipynb: notebook to compute stats from the results.

--- src/metadata.py
+++ src/metadata.py
@@ -12,5 +12,6 @@ GTEX_GWAS_PHENO_INFO = pd.read_csv(conf.GTEX_GWAS_PHENO_INFO_FILE, sep='\t', ind
 
 # Genes metadata
 BIOMART_GENES = pd.read_csv(conf.BIOMART_GENES_INFO_FILE, index_col='ensembl_gene_id')
+GENES_MAPPINGS = load_pickle(join(conf.GENES_METADATA_DIR, 'genes_mappings.pkl'))
 GENE_ID_TO_NAME_MAP = load_pickle(join(conf.GENES_METADATA_DIR, 'genes_mapping_simplified-0.pkl'))
 GENE_NAME_TO_ID_MAP = load_pickle(join(conf.GENES_METADATA_DIR, 'genes_mapping_simplified-1.pkl'))
