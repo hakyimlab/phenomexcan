57d311b553632e8fc78b1c2b1dc563ff14e27b93
Milton Pividori <miltondp@gmail.com>
2019-11-14 13:03:02 -0600

    Notebook to compute PhenomeXcan/Clinvar associations.

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "${1}" ]; then
+  echo "Specify notebook to run"
+  exit 1
+fi
+
+export PYTHONPATH=`pwd`/../src/
+jupyter nbconvert \
+  --ExecutePreprocessor.timeout=0 \
+  --to notebook \
+  --execute ${1}
+
