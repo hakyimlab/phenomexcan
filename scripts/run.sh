cfcaefb80633f8a45b9e33f82efa11b04f86d708
Milton Pividori <miltondp@uchicago.edu>
2019-11-25 22:29:19 -0600

    scripts/run.sh: improvements.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+set -e
 
 if [ -z "${1}" ]; then
   echo "Specify notebook to run"
   exit 1
 fi
 
+filename=$(basename -- "$1")
+filename="${filename%.*}"
+
 export PYTHONPATH=`pwd`/../src/
 jupyter nbconvert \
-  --ExecutePreprocessor.timeout=0 \
-  --to notebook \
-  --execute ${1}
+  --to python \
+  --output "$filename" \
+  ${1}
+
+ipython "${1%.*}".py
+
+rm "${1%.*}".py
 
