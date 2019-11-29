3919dcb893b80af5b6763aae1ae335ead30170ce
Milton Pividori <miltondp@gmail.com>
2019-11-29 00:13:46 -0600

    scripts/200_webapp: PostgreSQL scripts updated.

--- scripts/200_webapp/postgresql/load_smultixcan.sql
+++ scripts/200_webapp/postgresql/load_smultixcan.sql
@@ -7,6 +7,8 @@ create table smultixcan (
     pheno_desc varchar(225) not null,
     pheno_source varchar(20) not null,
     pvalue double precision not null,
+    dir_effect_most_signif smallint not null,
+    dir_effect_consensus smallint null,
     n smallint not null,
     n_indep smallint not null,
     p_i_best double precision not null,
@@ -15,7 +17,8 @@ create table smultixcan (
     primary key(pheno_desc, gene)
 );
 
-\copy smultixcan from /mnt/tmp/output_full.tsv with delimiter as E'\t' csv header;
+-- the file loaded here is the one generated with the script combine_smultixcan_results.sh
+\copy smultixcan from /mnt/tmp/output.tsv with delimiter as E'\t' csv header;
 
 CREATE INDEX pvalue_idx ON smultixcan (pvalue);
 CREATE INDEX pheno_desc_pvalue_idx ON smultixcan (pheno_desc, pvalue);
