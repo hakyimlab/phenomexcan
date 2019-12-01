616835a8d8f3a7a2a14d66950f05823333dde5e1
Milton Pividori <miltondp@gmail.com>
2019-12-01 00:23:17 -0600

    shinyapp: added new direction of effect columns.

--- scripts/200_webapp/shinyapp/helpers.R
+++ scripts/200_webapp/shinyapp/helpers.R
@@ -52,7 +52,9 @@ to_sql_list <- function(s) { sprintf("(values %s)", toString(sprintf("('%s')", s
 get_results_from_data_db_ <- function(input, table_sme, table_g) {
   CLAUSE <- "
 SELECT 
-  pheno_desc as phenotype, gene_name, pheno_source as type, band, gene, rcp as colocalization, pvalue, n as n_tissues, n_indep, t_i_best as best_tissue, p_i_best as best_tissue_p
+  pheno_desc as phenotype, gene_name, pheno_source as type, band, gene, rcp as colocalization,
+  dir_effect_most_signif as dir_eff_signif, dir_effect_consensus as dir_eff_cons, pvalue, n as n_tissues, n_indep,
+  t_i_best as best_tissue, p_i_best as best_tissue_p
 FROM smultixcan
 WHERE 1=1 
 " %>% glue::glue()
