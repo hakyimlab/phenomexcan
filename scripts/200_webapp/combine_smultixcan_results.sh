9054e776a8314b6d7d9512be1ff6485b1dfb3ad2
Milton Pividori <miltondp@gmail.com>
2019-12-26 11:55:20 -0600

    scripts/200_webapp: settings.sh to store global parameters.

--- scripts/200_webapp/combine_smultixcan_results.sh
+++ scripts/200_webapp/combine_smultixcan_results.sh
@@ -3,21 +3,17 @@
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH="${SCRIPT_DIR}/../../src/"
 
+. settings.sh
+
 read -r -d '' COMMAND << EOM
 python ${SCRIPT_DIR}/process_smultixcan.py \
   --smultixcan-file {} \
-  --fastenloc-h5-file /mnt/phenomexcan_base/gene_assoc/fastenloc-torus-rcp.h5 \
-  --spredixcan-most_signif-dir-effect-h5-file /mnt/phenomexcan_base/gene_assoc/spredixcan-mashr-effect_direction-most_signif.h5 \
-  --spredixcan-consensus-dir-effect-h5-file /mnt/phenomexcan_base/gene_assoc/spredixcan-mashr-effect_direction-consensus.h5 \
-  --phenotypes-info-file /mnt/phenomexcan_base/deliverables/phenotypes_info.tsv.gz \
-  --genes-info-file /mnt/phenomexcan_base/deliverables/genes_info.tsv.gz
+  --fastenloc-h5-file ${FASTENLOC_HDF5_FILE} \
+  --spredixcan-most_signif-dir-effect-h5-file ${SPREDIXCAN_DIRECTION_EFFECT_MOST_SIGNIF_HDF5_FILE} \
+  --spredixcan-consensus-dir-effect-h5-file ${SPREDIXCAN_DIRECTION_EFFECT_CONSENSUS_HDF5_FILE} \
+  --phenotypes-info-file ${PHENOTYPES_INFO_FILE} \
+  --genes-info-file ${GENES_INFO_FILE}
 EOM
-
-export SMULTIXCAN_RESULTS_DIR_00="/mnt/phenomexcan_base/results/smultixcan/rapid_gwas_project"
-export SMULTIXCAN_PATTERN_00='^smultixcan_(?P<code>.*)_ccn30\.tsv\.gz$'
-
-export SMULTIXCAN_RESULTS_DIR_01="/mnt/phenomexcan_base/results/smultixcan/gtex_gwas"
-export SMULTIXCAN_PATTERN_01='^(?P<code>.*)_smultixcan_imputed_gwas_gtexv8mashr_ccn30\.txt$'
 
 OUTPUT_FILE="${1}"
 if [ -z "${OUTPUT_FILE}" ]; then
--- scripts/200_webapp/combine_spredixcan_results.sh
+++ scripts/200_webapp/combine_spredixcan_results.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
+. settings.sh
+
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH="${SCRIPT_DIR}/../../src/"
-
-export SMULTIXCAN_RESULTS_DIR_00="/mnt/phenomexcan_base/results/smultixcan/rapid_gwas_project"
-export SMULTIXCAN_PATTERN_00='^smultixcan_(?P<code>.*)_ccn30\.tsv\.gz$'
-
-export SMULTIXCAN_RESULTS_DIR_01="/mnt/phenomexcan_base/results/smultixcan/gtex_gwas"
-export SMULTIXCAN_PATTERN_01='^(?P<code>.*)_smultixcan_imputed_gwas_gtexv8mashr_ccn30\.txt$'
 
 OUTPUT_FOLDER="${1}"
 if [ -z "${OUTPUT_FOLDER}" ]; then
@@ -33,20 +29,20 @@ read -r -d '' COMMAND << EOM
 python ${SCRIPT_DIR}/process_spredixcan.py \
   --phenotype-id "{}" \
   --tissue-id ${TISSUE_ID} \
-  --spredixcan-hdf5-folder /mnt/phenomexcan_base/gene_assoc/spredixcan \
-  --phenotypes-info-file /mnt/phenomexcan_base/deliverables/phenotypes_info.tsv.gz \
-  --tissues-info-file /mnt/phenomexcan_base/deliverables/tissues.tsv \
-  --genes-info-file /mnt/phenomexcan_base/deliverables/genes_info.tsv.gz
+  --spredixcan-hdf5-folder ${SPREDIXCAN_HDF5_FOLDER} \
+  --phenotypes-info-file ${PHENOTYPES_INFO_FILE} \
+  --tissues-info-file ${TISSUES_FILE} \
+  --genes-info-file ${GENES_INFO_FILE}
 EOM
 
     OUTPUT_FILE="${OUTPUT_FOLDER}/${TISSUE_NAME}.tsv"
 
     # add header
-    zcat /mnt/phenomexcan_base/deliverables/phenotypes_info.tsv.gz | tail -n +2 | head -1 | cut -f1 | \
+    zcat ${PHENOTYPES_INFO_FILE} | tail -n +2 | head -1 | cut -f1 | \
       parallel -j1 "${COMMAND} | head -1 > ${OUTPUT_FILE}"
 
     # add data
-    zcat /mnt/phenomexcan_base/deliverables/phenotypes_info.tsv.gz | tail -n +2 | cut -f1 | \
+    zcat ${PHENOTYPES_INFO_FILE} | tail -n +2 | cut -f1 | \
       parallel -j${N_JOBS} "${COMMAND} --no-header" >> ${OUTPUT_FILE}
   done
-} < /mnt/phenomexcan_base/deliverables/tissues.tsv
+} < ${TISSUES_FILE}
