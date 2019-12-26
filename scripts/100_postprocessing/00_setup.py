3e2ea1c3058b36cbbb8f9de2b691e4b84e0cd89a
Milton Pividori <miltondp@gmail.com>
2019-12-26 10:41:01 -0600

    00_setup.py improvements.

--- scripts/100_postprocessing/00_setup.py
+++ scripts/100_postprocessing/00_setup.py
@@ -1,10 +1,18 @@
 import os
+import hashlib
 import argparse
 
 import settings as conf
 
 
-def create_directories():
+def _check_md5(expected_md5, filepath):
+    with open(filepath, 'rb') as f:
+        current_md5 = hashlib.md5(f.read()).hexdigest()
+        assert expected_md5 == current_md5, f'md5 failed for "{filepath}"'
+    print(f'md5 file ok for {filepath}')
+
+
+def _create_directories():
     os.makedirs(conf.BASE_DIR, exist_ok=True)
     os.makedirs(conf.DATA_DIR, exist_ok=True)
     os.makedirs(conf.GTEX_DIR, exist_ok=True)
@@ -15,7 +23,7 @@ def create_directories():
 def download_mashr_models():
     output_file = os.path.join(conf.TMP_DIR, 'mashr_eqtl.tar')
     os.system(f'wget https://zenodo.org/record/3518299/files/mashr_eqtl.tar?download=1 -O {output_file}')
-    os.system(f'echo "87f3470bf2676043c748b684fb35fa7d {output_file}" | md5sum -c')
+    _check_md5('87f3470bf2676043c748b684fb35fa7d', output_file)
     os.system(f'tar -xf {output_file} -C {conf.TMP_DIR}')
     os.system(f'mv {conf.TMP_DIR}/eqtl/mashr/ {conf.GTEX_MODELS_DIR}')
 
@@ -24,7 +32,7 @@ def download_rapid_gwas_pheno_info():
     final_output_file = conf.RAPID_GWAS_PHENO_INFO_FILE
     output_file = os.path.join(conf.TMP_DIR, os.path.basename(final_output_file))
     os.system(f'wget https://uchicago.box.com/shared/static/46gsi3lrrsw030uircn4fesjcrn4898q.gz -O {output_file}')
-    os.system(f'echo "cba910ee6f93eaed9d318edcd3f1ce18 {output_file}" | md5sum -c')
+    _check_md5('cba910ee6f93eaed9d318edcd3f1ce18', output_file)
     os.system(f'mv {output_file} {final_output_file}')
 
 
@@ -32,7 +40,7 @@ def download_gtex_gwas_pheno_info():
     final_output_file = conf.GTEX_GWAS_PHENO_INFO_FILE
     output_file = os.path.join(conf.TMP_DIR, os.path.basename(final_output_file))
     os.system(f'wget https://uchicago.box.com/shared/static/hvuqanqclnj5wz8xap4xx9jpyrms7bqr.tsv -O {output_file}')
-    os.system(f'echo "982434335f07acb1abfb83e57532f2c0 {output_file}" | md5sum -c')
+    _check_md5('982434335f07acb1abfb83e57532f2c0', output_file)
     os.system(f'mv {output_file} {final_output_file}')
 
 
@@ -40,7 +48,7 @@ def download_genes_information():
     final_output_file = conf.BIOMART_GENES_INFO_FILE
     output_file = os.path.join(conf.TMP_DIR, os.path.basename(final_output_file))
     os.system(f'wget https://uchicago.box.com/shared/static/qxgjh6kpa8543s99tlclhil74cd4536k.gz -O {output_file}')
-    os.system(f'echo "c4d74e156e968267278587d3ce30e5eb {output_file}" | md5sum -c')
+    _check_md5('c4d74e156e968267278587d3ce30e5eb', output_file)
     os.system(f'mv {output_file} {final_output_file}')
 
 
@@ -48,7 +56,7 @@ def download_clinvar_data():
     final_output_file = conf.CLINVAR_DATA_FILE
     output_file = os.path.join(conf.TMP_DIR, os.path.basename(final_output_file))
     os.system(f'wget https://uchicago.box.com/shared/static/g83rtsctb3borlk5p815mcki27jrje0e -O {output_file}')
-    os.system(f'echo "ccfd90344cc59c9869bf6e012c0e5c77 {output_file}" | md5sum -c')
+    _check_md5('ccfd90344cc59c9869bf6e012c0e5c77', output_file)
     os.system(f'mv {output_file} {final_output_file}')
 
 
@@ -63,7 +71,7 @@ def download_fastenloc_tissues():
 def download_smultixcan_results_for_rapid_gwas_project():
     output_file = os.path.join(conf.TMP_DIR, 'smultixcan_mashr.tar')
     os.system(f'wget https://uchicago.box.com/shared/static/prq9o1vuwtba4ktfi1tm1hjqfiy8qvar.tar -O {output_file}')
-    os.system(f'echo "6900f6da03d6b430f942a32ec41124ba {output_file}" | md5sum -c')
+    _check_md5('6900f6da03d6b430f942a32ec41124ba', output_file)
     os.system(f'tar -xf {output_file} -C {conf.TMP_DIR}')
     os.system(f'mv {conf.TMP_DIR}/smultixcan/ {conf.SMULTIXCAN_RESULTS_DIR["RapidGWASProject"]}')
 
@@ -71,15 +79,19 @@ def download_smultixcan_results_for_rapid_gwas_project():
 def download_smultixcan_results_for_gtex_gwas():
     output_file = os.path.join(conf.TMP_DIR, 'gtex_gwas-42_traits-smultixcan.tar.gz')
     os.system(f'wget https://uchicago.box.com/shared/static/qi8m6mdxdyss42ui6dsukpuemh107yfv.gz -O {output_file}')
-    os.system(f'echo "2b1ade011506f87fe632ad56c6c99357 {output_file}" | md5sum -c')
+    _check_md5('2b1ade011506f87fe632ad56c6c99357', output_file)
     os.system(f'tar -xf {output_file} -C {conf.TMP_DIR}')
-    os.system(f'mv {conf.TMP_DIR}/smultixcan/ {conf.SMULTIXCAN_RESULTS_DIR["RapidGWASProject"]}')
+    os.system(f'mv {conf.TMP_DIR}/smultixcan/ {conf.SMULTIXCAN_RESULTS_DIR["GTEX_GWAS"]}')
+
+
+def download_fastenloc_results_for_rapid_gwas_project():
+    pass
 
 
 available_actions = {'all': None, }
 local_items = list(locals().items())
 for key, value in local_items:
-    if callable(value) and value.__module__ == __name__ and key != 'create_directories':
+    if callable(value) and value.__module__ == __name__ and not key.startswith('_'):
         available_actions[key] = value
 
 
@@ -87,13 +99,17 @@ parser = argparse.ArgumentParser(description='PhenomeXcan data setup.')
 parser.add_argument('actions', choices=available_actions, nargs="+")
 args = parser.parse_args()
 
-create_directories()
+_create_directories()
 
 actions_to_run = args.actions
 if 'all' in args.actions:
     actions_to_run = list(available_actions.keys())
 
-
+print(actions_to_run)
 for k in actions_to_run:
+    if k == 'all':
+        continue
+
     print(f'Running {k}')
     available_actions[k]()
+
