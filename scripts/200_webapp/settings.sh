82fb9fad4257a888f75408a5517f2b9eeb1dfce1
Milton Pividori <miltondp@gmail.com>
2020-01-07 14:48:26 -0600

    scripts/200_webapp: README.md updated.

--- scripts/200_webapp/settings.sh
+++ scripts/200_webapp/settings.sh
@@ -6,7 +6,7 @@ export GENES_INFO_FILE="/mnt/phenomexcan_base/deliverables/genes_info.tsv.gz"
 export SMULTIXCAN_RESULTS_DIR_00="/mnt/phenomexcan_base/results/smultixcan/rapid_gwas_project"
 export SMULTIXCAN_PATTERN_00='^smultixcan_(?P<code>.*)_ccn30\.tsv\.gz$'
 export SMULTIXCAN_RESULTS_DIR_01="/mnt/phenomexcan_base/results/smultixcan/gtex_gwas"
-export SMULTIXCAN_PATTERN_01='^(?P<code>.*)_smultixcan_imputed_gwas_gtexv8mashr_ccn30\.txt$'
+export SMULTIXCAN_PATTERN_01='^(?P<code>.*)_smultixcan_imputed_gwas_gtexv8mashr_ccn30\.txt\.gz$'
 
 export SPREDIXCAN_HDF5_FOLDER="/mnt/phenomexcan_base/gene_assoc/spredixcan"
 export SPREDIXCAN_DIRECTION_EFFECT_MOST_SIGNIF_HDF5_FILE="/mnt/phenomexcan_base/gene_assoc/spredixcan-mashr-effect_direction-most_signif.h5"
@@ -15,6 +15,7 @@ export SPREDIXCAN_DIRECTION_EFFECT_CONSENSUS_HDF5_FILE="/mnt/phenomexcan_base/ge
 export FASTENLOC_HDF5_FILE="/mnt/phenomexcan_base/gene_assoc/fastenloc-torus-rcp.h5"
 
 # PostgreSQL configuration
+# the db user must be an admin user
 export DBUSER="ukbrest"
 export DBHOST='localhost'
 export DBPORT=7432
