9a94464d6a9983c1a0b2b4158a34db4aa4ec7346
Milton Pividori <miltondp@uchicago.edu>
2020-02-11 21:42:03 -0600

    run_nbs.sh allows to specify IP address.

--- run_nbs.sh
+++ run_nbs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-PYTHONPATH=`pwd`/src jupyter lab --ip 127.0.0.1 --port 8888 --no-browser
+IP="127.0.0.1"
+if [ ! -z "${1}" ]; then
+  IP="${1}"
+  echo "Using IP: ${1}"
+fi
+
+PYTHONPATH=`pwd`/src jupyter lab --ip ${IP} --port 8888 --no-browser
 
